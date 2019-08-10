f56123e9370232eca85a0fc92f6054beb8af49a6
sebastienserre <sebastien@thivinfo.com>
2019-08-10 17:28:00 +0200

    add select with cpt

--- class/class-options.php
+++ class/class-options.php
@@ -3,6 +3,8 @@
 namespace WP_PERICLES\Options;
 
 use function add_action;
+use function get_post_type;
+use function var_dump;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -11,6 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Options {
 	public function __construct() {
 		add_action( 'plugins_loaded', array( $this, 'add_option_page' ), 999 );
+		add_filter( 'acf/load_field/name=wppericles_existing_cpt', [ $this, 'load_cpt_list']);
 	}
 
 	public function add_option_page() {
@@ -26,7 +29,11 @@ class Options {
 				)
 			);
 		}
+	}
 
+	public function load_cpt_list( $field ){
+		$field['choices'] = get_post_types();
+		return $field;
 	}
 }
 
