a41600ad071f478edd67dc4cf240a4747bbe1814
sebastienserre <sebastien@thivinfo.com>
2019-12-30 19:03:05 +0100

    add a textdomain to acf fields

--- wp-pericles-import.php
+++ wp-pericles-import.php
@@ -25,6 +25,7 @@ use function register_activation_hook;
 use function register_deactivation_hook;
 use function time;
 use function untrailingslashit;
+use function var_dump;
 use function wp_clear_scheduled_hook;
 use function wp_get_upload_dir;
 use function wp_next_scheduled;
@@ -92,6 +93,7 @@ class WPPericles {
 		add_action( 'acf/include_fields', [ $this, 'my_register_fields' ] );
 
 		add_filter( 'acf/settings/save_json', [ $this, 'acf_save_point' ] );
+		add_filter( 'acf/settings/l10n_textdomain', [ $this, 'acf_textdomain' ] );
 
 		register_activation_hook( __FILE__, [ $this, 'activation' ] );
 		register_deactivation_hook( __FILE__, [ $this, 'deactivation' ] );
@@ -197,6 +199,10 @@ class WPPericles {
 		// return
 		return $path;
 
+	}
+
+	public function acf_textdomain( $td ) {
+		return $td = 'wp-pericles-import';
 	}
 }
 
