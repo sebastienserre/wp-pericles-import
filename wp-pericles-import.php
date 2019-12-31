f2e0e9a558a3cb229428ccbb06b7440a3fa66c5d
sebastienserre <sebastien@thivinfo.com>
2019-12-31 16:27:07 +0100

    add templates

--- wp-pericles-import.php
+++ wp-pericles-import.php
@@ -5,7 +5,7 @@
 	Description: Connect your Real Estate Agency to WordPress!
 	Author: Sébastien SERRE
 	Author URI: https://thivinfo.com
-	Version: 1.2.1
+	Version: 1.3.0
 	Text Domain: wp-pericles-import
 	Domain Path: /languages
 	*/
